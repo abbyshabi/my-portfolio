9cefb5ceba23ececda2442dad686b868971e5585
Dammy1 <abbyshabi@gmail.com>
2025-09-27 21:52:30 -0400

    remove export from package.json

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   output: 'export', // create static HTML
   basePath:'',
-  assetPrefix: isProd ? '/my-portfolio/' : '',
+  assetPrefix: "",
   images: { unoptimized: true }, // GH Pages has no Next image optimizer
   trailingSlash: true,
   eslint: { ignoreDuringBuilds: true } // helps with GH Pages routing
